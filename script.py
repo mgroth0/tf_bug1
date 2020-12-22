5164a706d33255f3c300060e2a33dd4bbf556341
Matt Groth <mgroth49@gmail.com>
2020-12-22 12:17:49 -0500

    tf

--- script.py
+++ script.py
@@ -1,1 +1,11 @@
 print('hello world')
+import tensorflow as tf
+net = tf.keras.applications.InceptionResNetV2(
+    include_top=True,
+    weights=None,  # 'imagenet',
+    input_tensor=None,
+    input_shape=None,
+    pooling=None,
+    classes=2,  # 1000,
+    classifier_activation='softmax'
+)
